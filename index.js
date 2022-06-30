cf9049924c92246e7acb9a414d776e100719dd29
Julian <kausjulian@gmail.com>
2022-06-30 18:01:46 -0300

    config nodemailer

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ app.post('/sendmail',(req,res)=>{
     
     let mailOptions = {
         from:'billy.bauch52@ethereal.email',
-        to:data.email,
+        to:`${data.email}`,
         subject:'Confirmación de compra Tech Center',
-        html:'<h1 class="text-center" style="color: rgb(30, 144, 252)">Tech Center</h1><p> Gracias por tu compra en Tech Center! En breve nos pondremos en contacto con vos para indicarte los pasos a seguir</p>'
+        html:'<h1 class="text-center" style="color: rgb(30, 144, 252)">Tech Center</h1><p> Gracias por tu compra en Tech Center! En breve nos pondremos en contacto con vos para indicarte los pasos a seguir.</p>'
     }
     
     transporter.sendMail(mailOptions,function(error,info){
